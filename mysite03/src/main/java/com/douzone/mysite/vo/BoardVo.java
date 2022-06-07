af593a8bf9139444201e48c8454c61041e1c9621
seoin1102 <im_in_@naver.com>
2022-06-07 14:20:49 +0900

    mysite03

---
+++ mysite03/src/main/java/com/douzone/mysite/vo/BoardVo.java
@@ -0,0 +1,82 @@
+package com.douzone.mysite.vo;
+ 
+public class BoardVo {
+	private Long no;
+	private String title;
+	private String contents;
+	private Long hit;
+	private String regDate;
+	private Long gNo;
+	private Long oNo;
+	private int depth;
+	private Long userNo;
+	private String userName;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Long getHit() {
+		return hit;
+	}
+	public void setHit(Long hit) {
+		this.hit = hit;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public Long getgNo() {
+		return gNo;
+	}
+	public void setgNo(Long gNo) {
+		this.gNo = gNo;
+	}
+	public Long getoNo() {
+		return oNo;
+	}
+	public void setoNo(Long oNo) {
+		this.oNo = oNo;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	public Long getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(Long userNo) {
+		this.userNo = userNo;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	@Override
+	public String toString() {
+		return "BoardVo [no=" + no + ", title=" + title + ", contents=" + contents + ", hit=" + hit + ", regDate="
+				+ regDate + ", gNo=" + gNo + ", oNo=" + oNo + ", depth=" + depth + ", userNo=" + userNo + ", userName="
+				+ userName + "]";
+	}
+	
+	
+}
