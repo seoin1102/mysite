2eb5c2b5a2ab9036eee2e45bf362f653fe46eb91
seoin1102 <im_in_@naver.com>
2022-06-13 12:20:02 +0900

    AuthInterceptor

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -8,9 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.douzone.mysite.security.Auth;
 import com.douzone.mysite.service.UserService;
 import com.douzone.mysite.vo.UserVo;
 
+@Auth(role="ADMIN")
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -37,26 +39,17 @@ public class UserController {
 	public String login() {
 		return "user/login";
 	}
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(HttpSession session, UserVo vo, Model model) {
-		UserVo authUser = userService.getUser(vo);
-		if(authUser == null) {
-			model.addAttribute("result","fail");
-			model.addAttribute("email", vo.getEmail());
-			return "user/login";
-		}
-		
-		/*인증 처리*/
-		session.setAttribute("authUser", authUser);
-		return "redirect:/";
-	}
-	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
-		session.removeAttribute("authUser");
-		session.invalidate();
-		return "redirect:/";
+	
+	@RequestMapping(value = "/auth")
+	public void auth() {	
 	}
 	
+	@RequestMapping("/logout")
+	public void logout() {
+	}
+	
+	//@Auth(value= "hello", test =true)
+	@Auth(role="USER")
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String update(HttpSession session,  Model model) {
 		//  접근 제어(Access Control)
--- mysite03/src/main/java/com/douzone/mysite/security/LoginInterceptor.java
+++ mysite03/src/main/java/com/douzone/mysite/security/LoginInterceptor.java
@@ -2,6 +2,7 @@ package com.douzone.mysite.security;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -28,8 +29,10 @@ public class LoginInterceptor implements HandlerInterceptor {
 			return false;
 		}
 		/* session 처리 */
-		System.out.println(authUser);
+		HttpSession session =request.getSession(true);
+		session.setAttribute("authUser", authUser);
 		
+		response.sendRedirect(request.getContextPath());
 		return false;
 	}
 
