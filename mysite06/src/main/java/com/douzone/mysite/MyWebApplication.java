42dcce3bb65623e523aa57802c060db6eaeb78db
seoin1102 <im_in_@naver.com>
2022-06-24 00:42:12 +0900

    commit

--- mysite06/src/main/java/com/douzone/mysite/MyWebApplication.java
+++ mysite06/src/main/java/com/douzone/mysite/MyWebApplication.java
@@ -8,15 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MyWebApplication {
-
-	@RestController
-	public class HelloController2 {
-		@GetMapping("/hello")
-		public String hello() {
-			return "hello";
-		}
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(MyWebApplication.class, args);
 	}
